99ce7a5cc8cc48b080b095b1ef61bf98f3281e33
krygin <krygin.ia@gmail.com>
2024-04-14 16:27:22 +0300

    Initial commit

---
+++ app/src/main/java/ru/krygin/widget/MainActivity.kt
@@ -0,0 +1,46 @@
+package ru.krygin.widget
+
+import android.appwidget.AppWidgetManager
+import android.content.ComponentName
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ru.krygin.widget.ui.theme.WidgetDemoApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Button(onClick = ::requestPinAppWidget) {
+                Text(text = "Add Widget")
+            }
+        }
+    }
+
+    private fun requestPinAppWidget() {
+        val appWidgetManager = AppWidgetManager.getInstance(this)
+        val provider = ComponentName(this, DemoAppWidget::class.java)
+        appWidgetManager.requestPinAppWidget(provider, null, null)
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    WidgetDemoApplicationTheme {
+        Greeting("Android")
+    }
+}
